d327bae1b3c51ab9d8b1fd45bf98252ca95b5e5f
timothylam1228 <38665439+timothylam1228@users.noreply.github.com>
2021-11-20 01:55:31 +0800

    add enc method

--- main.py
+++ main.py
@@ -7,21 +7,29 @@ import PySimpleGUI as sg;
 sg.theme('Dark Blue 3')   # Add a little color to your windows
 fontSize = 20
 
-tab1_layout = [[sg.Text('DES', font="Helvetica "  + str(fontSize)) ,sg.Combo(['Select1', 'Select2'])],
-        [sg.Text('Input the key size'), sg.Input()],
-            [sg.OK(), sg.Cancel()]]
-tab2_layout =  [[sg.Text('AES', font="Helvetica "  + str(fontSize)) ,sg.Combo(['Select1', 'Select2'])],
+des_layout = [[sg.Text('DES', font="Helvetica "  + str(fontSize)) ,sg.Combo(['Select1', 'Select2'])],
         [sg.Text('Input the key size'), sg.Input()],
             [sg.OK(), sg.Cancel()]]
 
+triple_des_layout = [[sg.Text('Triple DES', font="Helvetica "  + str(fontSize)) ,sg.Combo(['Select1', 'Select2'])],
+        [sg.Text('Input the key size'), sg.Input()],
+            [sg.OK(), sg.Cancel()]]
 
-layout = [[sg.TabGroup([[sg.Tab('Tab 1', tab1_layout), sg.Tab('Tab 2', tab2_layout)]])]]
+aes_layout =  [[sg.Text('AES', font="Helvetica "  + str(fontSize)) ,sg.Combo(['Select1', 'Select2'])],
+        [sg.Text('Input the key size'), sg.Input()],
+            [sg.OK(), sg.Cancel()]]
+
+rsa_layout = [[sg.Text('RSA', font="Helvetica "  + str(fontSize)) ,sg.Combo(['Select1', 'Select2'])],
+        [sg.Text('Input the key size'), sg.Input()],
+            [sg.OK(), sg.Cancel()]]
+
+layout = [[sg.TabGroup([[sg.Tab('DES', des_layout), sg.Tab('Triple DES', triple_des_layout),sg.Tab('AES',aes_layout),sg.Tab('RSA',rsa_layout)]])]]
 
 window = sg.Window('Window Title', layout)
 
 while True:
     event, values = window.read()
-
+    print(event)
     if event == 'OK':
         print(values)
     if event in (sg.WIN_CLOSED, 'Cancel'):
