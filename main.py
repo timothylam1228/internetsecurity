80f8214ec82e50688af1718264b7966572173df4
timothylam1228 <38665439+timothylam1228@users.noreply.github.com>
2021-11-22 18:58:48 +0800

    new hash method

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import PySimpleGUI as sg
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad
-
+from passlib.hash import des_crypt
 from base64 import b64encode
-
+from passlib.crypto import des
+import random
 
 import codecs
 sg.theme('Dark Blue 3')   # Add a little color to your windows
@@ -20,7 +21,7 @@ def generateDesKey():
 
 
 des_layout = [[sg.Text('DES', font="Helvetica"+str(fontSize)) ,sg.Combo(['ECE','CBC','CFB','OFB','CTR'])],
-              [sg.Text('Key size is fixed at 56 bytes'),sg.Input(disabled=True,key = 'des_key'),sg.Button(target=(1,1),button_text="Generate Key",key='genDesKey')],
+              [sg.Text('Key size is fixed at 56 bytes'),sg.Input(disabled=True,key = 'des_key'),sg.Button(button_text="Generate Key",key='genDesKey')],
               [sg.Text('Input the message to encrypt'), sg.Input()]
               ]
 
@@ -40,28 +41,29 @@ layout = [[sg.TabGroup([[sg.Tab('DES', des_layout), sg.Tab('Triple DES', triple_
 window = sg.Window('Window Title', layout)
 
 while True:
-    hashfile = open('hashed.txt','a')
-    hashfile.seek(0)
     event, values = window.read()
     if event == 'genDesKey':
         generatebytes = get_random_bytes(8)
-        print(generatebytes)
-        window['des_key'].update(generatebytes)
+        window['des_key'].update(str(generatebytes))
     if event == 'OK':
-        print(values)
+        hashfile = open('example.hash', 'w').close()
+        hashfile = open('example.hash', 'a')
         tab_page = values['Tab']
         if(tab_page == 'DES'):
-            with open('dict.txt', 'r+') as f:
+            with open('example.dict', 'r+') as f:
                 dict = (f.read().splitlines())
-                key = values['des_key']
-                iv = get_random_bytes(8)
+                key = generatebytes
+                salt = random.getrandbits(24)
                 for plaintext in dict:
                     bytesplaintext = bytes(plaintext,'utf-8')
-                    bytesplaintext = pad(bytesplaintext, 8)
-                    chiper = DES.new(key, DES.MODE_CBC,iv)
-                    chipertext = chiper.encrypt(bytesplaintext)
-                    chipertext = b64encode(chipertext).decode()
-                    hashfile.write(chipertext)
+                    bytesplaintext = pad(bytesplaintext, 8,style='pkcs7')
+                    # chiper = DES.new(key, DES.MODE_ECB)
+                    # chipertext = chiper.encrypt(bytesplaintext)
+                    # chipertext = b64encode(chipertext).decode()
+                    print(bytesplaintext)
+                    chipertext = des.des_encrypt_block(key,bytesplaintext,salt)
+                    print(chipertext)
+                    hashfile.write(str(chipertext))
                     hashfile.write('\n')
                     # print(str(chipertext), file=hashfile)  # Python 3.x
 
