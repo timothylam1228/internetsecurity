73f956b5a9181d7d31ffecc0e701f0d96b773648
kaking1126 <kakingchow33@gmail.com>
2021-11-23 16:32:40 +0800

    RSA

--- main.py
+++ main.py
@@ -3,18 +3,19 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import base64
 import PySimpleGUI as sg
+from Crypto import Random
 from Crypto.Random import get_random_bytes
-from Crypto.Cipher import DES
+from Crypto.Cipher import PKCS1_v1_5 as PKCS1_cipher
 # from Crypto.Util.Padding import pad
 from base64 import b64encode
 from Crypto.Util.Padding import pad, unpad
+from Crypto.PublicKey import RSA
 
-import crypt
 # from passlib.hash import des_crypt
 import random
 from os import urandom
-import os
 
 import codecs
 sg.theme('Dark Blue 3')   # Add a little color to your windows
@@ -68,7 +69,7 @@ while True:
                 for plaintext in dict:
                     # plaintext = b'passwor1 and 1 wqsd sds'
                     plaintext = (bytes(plaintext,'utf8'))
-                    cipher = DES.new(key, DES.MODE_ECB)
+                    cipher = PKCS1_cipher.new(key, PKCS1_cipher.MODE_ECB)
                     # plaintext = bytes(plaintext,'utf-8')
                     msg = cipher.encrypt(pad(plaintext,8))
                     hashfile.write(str(b64encode(plaintext).decode('utf8')))
@@ -88,6 +89,37 @@ while True:
         elif(tab_page == 'RSA'):
             print(values)
             print('RSA')
+            random_generator = Random.new().read
+            rsa = RSA.generate(2048, random_generator)
+            
+            private_key = rsa.exportKey()
+
+            public_key = rsa.publickey().exportKey()
+
+            with open("public.txt", 'wb') as f:
+                f.write(public_key)
+            with open("private.txt", 'wb') as f:
+                f.write(private_key)
+
+            message = "sth"
+
+            with open("public.txt") as f:
+                key = f.read()
+                pub_key = RSA.importKey(str(key))
+                cipher = PKCS1_cipher.new(pub_key)
+                rsa_text = base64.b64encode(cipher.encrypt(bytes(message.encode("utf8"))))
+                print('encrypted')
+                print(rsa_text.decode('utf-8'))
+
+            with open ("private.txt") as f:
+                key = f.read()
+                pri_key = RSA.importKey(key)
+                cipher = PKCS1_cipher.new(pri_key)
+                back_text = cipher.decrypt(base64.b64decode(rsa_text),0)
+                print('decrypted')
+                print(back_text.decode('utf-8'))
+
+
 
     if event in (sg.WIN_CLOSED, 'Cancel'):
         break
