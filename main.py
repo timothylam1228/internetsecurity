a2df1d4ad46a419718af3811fc1a7874c7e49e7e
timothylam1228 <38665439+timothylam1228@users.noreply.github.com>
2021-11-23 21:21:18 +0800

    =MASTER MERGE RSA

--- main.py
+++ main.py
@@ -2,13 +2,21 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import base64
 
 import PySimpleGUI as sg
+from Crypto import Random
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import DES
+from Crypto.Cipher import PKCS1_v1_5 as PKCS1_cipher
+from base64 import b64encode
+
 # from Crypto.Util.Padding import pad
 from base64 import b64encode
 from Crypto.Util.Padding import pad, unpad
+from Crypto.PublicKey import RSA
+
+import random
 
 import crypt
 # from passlib.hash import des_crypt
@@ -85,12 +93,41 @@ while True:
             print(values)
             iv = get_random_bytes(16)
             print('AES')
-        elif(tab_page == 'RSA'):
+        elif (tab_page == 'RSA'):
             print(values)
             print('RSA')
+            random_generator = Random.new().read
+            rsa = RSA.generate(2048, random_generator)
+
+            private_key = rsa.exportKey()
+
+            public_key = rsa.publickey().exportKey()
+
+            with open("public.txt", 'wb') as f:
+                f.write(public_key)
+            with open("private.txt", 'wb') as f:
+                f.write(private_key)
+
+            message = "sth"
+
+            with open("public.txt") as f:
+                key = f.read()
+                pub_key = RSA.importKey(str(key))
+                cipher = PKCS1_cipher.new(pub_key)
+                rsa_text = base64.b64encode(cipher.encrypt(bytes(message.encode("utf8"))))
+                print('encrypted')
+                print(rsa_text.decode('utf-8'))
+
+            with open("private.txt") as f:
+                key = f.read()
+                pri_key = RSA.importKey(key)
+                cipher = PKCS1_cipher.new(pri_key)
+                back_text = cipher.decrypt(base64.b64decode(rsa_text), 0)
+                print('decrypted')
+                print(back_text.decode('utf-8'))
 
     if event in (sg.WIN_CLOSED, 'Cancel'):
-        break
+            break
 
 
 
@@ -99,7 +136,6 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-print(testing)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
